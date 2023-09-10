d02aedacb8723533f1a8da2ac63aea856556e69d
sergiuchilat <chilatsergiu@gmail.com>
2023-09-10 12:23:02 +0300

    CRUD sample

--- src/index.js
+++ src/index.js
@@ -18,6 +18,18 @@ app.get('/users', async(req, res) => {
     res.json(await usersService.get(1));
 })
 
+app.post('/users', async(req, res) => {
+    res.json(await usersService.create(req.body));
+})
+
+app.put('/users/:id', async(req, res) => {
+    res.json(await usersService.update(req.body, Number(req.params.id)));
+})
+
+app.delete('/users/:id', async(req, res) => {
+    res.json(await usersService.remove(Number(req.params.id)));
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- src/services/UsersServices.js
+++ src/services/UsersServices.js
@@ -16,6 +16,50 @@ async function get(page = 1) {
     }
 }
 
+async function create(user) {
+    const result = await db.query(
+        `INSERT INTO users 
+            (username, password) 
+        VALUES 
+            ('${user.username}', '${user.password}')`
+    )
+    if(!result.affectedRows){
+        return {"error": "Not created"}
+    }
+    return user;
+}
+
+async function update(user, id) {
+    const result = await db.query(
+        `UPDATE users SET
+            username = '${user.username}', 
+            password = '${user.password}'
+        WHERE
+            id = ${id}
+        `
+    )
+    if(!result.affectedRows){
+        return {"error": "Not updated"}
+    }
+    return user;
+}
+
+async function remove(id) {
+    const result = await db.query(`
+        DELETE FROM users 
+        WHERE
+            id = ${id}
+        `
+    )
+    if(!result.affectedRows){
+        return {"error": "Not updated"}
+    }
+    return {};
+}
+
 module.exports = {
-    get
+    get,
+    create,
+    update,
+    remove
 }
